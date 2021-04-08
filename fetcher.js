905c2b2ec69cc9a4f2bcc2289494cd2c43360b2b
Dylan <dcren2010@gmail.com>
2021-04-08 01:29:37 -0400

    fn updated for if file exist or not

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,46 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
 const args = process.argv.slice(2, 4)
-// console.log(args);
+
+const writeFile = (filePath, body) => {
+	fs.writeFile(filePath, body, (err) => {
+    if (err) {
+			console.log(('Failed to write to file, please check local file path'));
+			return;
+    } else {
+      const stats = fs.statSync(filePath);
+      const fileSizeInBytes = stats.size;
+      console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${filePath}`);
+    }
+  });
+}
 
 const fetch = (argsArr) => {
 	request(argsArr[0], (error, response, body) => {
 		if (error) {
-			console.log(`Error happened, please check URL`);
-			return;
-		}
- console.log('statusCode:', response && response.statusCode);
-		fs.writeFile(argsArr[1], body, (error) => {
-      if (error) {
-				console.log('Failed to write to file, please check local file path');
-				return;
-			} 
-      console.log(`Downloaded and saved 3261 bytes to ${argsArr[1]}`);
-    });
+			throw Error(`Error happened, please check URL`);
+		} else if (response.statusCode !== 200) {
+			throw Error('Page access failed')
+		};
+		
+		fs.access(argsArr[1], fs.constants.F_OK, err => {
+			if (err) {
+				writeFile(argsArr[1], body);
+			} else {
+					const rl = readline.createInterface({
+					input: process.stdin,
+					output: process.stdout
+					});
+				rl.question('File already exists, type Y to overwrite, or cancel by typing anything else...', answer => {
+					if (answer === 'Y') {
+							writeFile(argsArr[1], body);
+					}
+					rl.close();
+					});
+			}
+		})
 });
 }
 
