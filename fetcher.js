7e04991efa861cac294b51e738fea456bafc41d3
Dylan <dcren2010@gmail.com>
2021-04-08 01:38:55 -0400

    code clean up

--- fetcher.js
+++ fetcher.js
@@ -21,28 +21,26 @@ const fetch = (argsArr) => {
 	request(argsArr[0], (error, response, body) => {
 		if (error) {
 			throw Error(`Error happened, please check URL`);
-		} else if (response.statusCode !== 200) {
-			throw Error('Page access failed')
 		};
-		
+		console.log(response.statusCode);
 		fs.access(argsArr[1], fs.constants.F_OK, err => {
 			if (err) {
 				writeFile(argsArr[1], body);
 			} else {
-					const rl = readline.createInterface({
+				const rl = readline.createInterface({
 					input: process.stdin,
 					output: process.stdout
-					});
+				});
 				rl.question('File already exists, type Y to overwrite, or cancel by typing anything else...', answer => {
 					if (answer === 'Y') {
-							writeFile(argsArr[1], body);
+						writeFile(argsArr[1], body);
 					}
 					rl.close();
-					});
+				});
 			}
-		})
-});
-}
+		});
+	});
+};
 
 fetch(args)
 
