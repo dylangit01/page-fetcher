3f7786b7cf34d4aaae9a0bb6d8a3d785f0df06b5
Dylan <dcren2010@gmail.com>
2021-04-09 23:26:16 -0400

    code clean up

--- fetcherBackup.js
+++ fetcherBackup.js
@@ -2,38 +2,30 @@ const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
 
-const args = process.argv.slice(2, 4)
+const args = process.argv.slice(2, 4);
 
 const writeFile = (filePath, body) => {
-	fs.writeFile(filePath, body, (err) => {
+  fs.writeFile(filePath, body, (err) => {
     if (err) {
-			console.log(('Failed to write to file, please check local file path'));
-			return;
+      console.log('Failed to write to file, please check local file path');
+      return;
     } else {
       const stats = fs.statSync(filePath);
       const fileSizeInBytes = stats.size;
       console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${filePath}`);
     }
   });
-}
+};
 
-const fetch = (argsArr, callback) => {
-	request(argsArr[0], 'utf-8', (error, response, body) => {
-		if (error) {
-			callback(`Error happened, please check if URL correct`, null)
-		} else {
-			callback(null, body, argsArr[1])
-		}
-	})
-}
-
-fetch(args, (error, content, filePath) => {
-	if (error) {
-		console.log(error);
-	} else {
-		fs.access(filePath, fs.constants.F_OK, (err) => {
+const fetch = (argsArr) => {
+  request(argsArr[0], 'utf-8', (error, response, body) => {
+    if (error) {
+      throw Error(`Error happened, please check URL`);
+    }
+    console.log(response.statusCode);
+    fs.access(argsArr[1], fs.constants.F_OK, (err) => {
       if (err) {
-        writeFile(filePath, content);
+        writeFile(argsArr[1], body);
       } else {
         const rl = readline.createInterface({
           input: process.stdin,
@@ -41,12 +33,13 @@ fetch(args, (error, content, filePath) => {
         });
         rl.question('File already exists, type Y to overwrite, or cancel by typing anything else...', (answer) => {
           if (answer === 'Y') {
-            writeFile(filePath, content);
+            writeFile(argsArr[1], body);
           }
           rl.close();
         });
       }
     });
-	}
-})
+  });
+};
 
+fetch(args);
